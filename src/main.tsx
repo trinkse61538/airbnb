1a189df0427f9766a42301eb9a2a1551c2fbecdf
trinkse61538 <32486887+trinkse61538@users.noreply.github.com>
2026-07-27 16:05:08 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import './index.css';
 import { ApartmentDataProvider } from './data/ApartmentDataProvider.tsx';
 import { registerPWA } from './pwa.ts';
 import { LanguageProvider } from './i18n.tsx';
+import ParkingExtension from './components/ParkingExtension.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
       <ApartmentDataProvider>
         <App />
+        <ParkingExtension />
       </ApartmentDataProvider>
     </LanguageProvider>
   </StrictMode>,
