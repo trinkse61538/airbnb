dac8a02caffbde8d4aaff9c967fbb8c5dd808590
trinkse61538 <32486887+trinkse61538@users.noreply.github.com>
2026-07-28 09:34:34 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'apartment-control-pwa-v3-0-19';
+const CACHE_NAME = 'apartment-control-pwa-v3-0-20';
 const CACHE_PREFIX = 'apartment-control-pwa-';
 
 function scopedUrl(path = '') {
